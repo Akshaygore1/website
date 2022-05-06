8f01579c92cf2de374f00eea3a804f3fe8c75218
Akshay Gore <92959398+Akshaygore1@users.noreply.github.com>
2022-05-06 10:46:57 +0530

    Update Front.jsx
    full stack

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -16,7 +16,7 @@ function Front() {
                     Akshay Gore
                 </p>
                 <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2'>
-                     Front-End Developer
+                     Full Stack Developer
                 </p>
         <div className="flex align-center justify-center mt-4">
 
